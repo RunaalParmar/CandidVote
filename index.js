9523c74061d9df372339bba268a61dba8077a657
Sriram Sivaraman <sriramsivaraman@Srirams-MacBook-Air.local>
2022-02-26 11:39:26 -0500

    adding gitignore file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const createServer = require("./servers/user_server") // new
+const createServer = require("./servers/user_server") 
 
 mongoose
 	.connect("mongodb://localhost:27017/CandidVote", { useNewUrlParser: true })
 	.then(() => {
-		const app = createServer() // new
+		const app = createServer() 
 		app.listen(5000, () => {
 			console.log("Server has started!")
 		})
