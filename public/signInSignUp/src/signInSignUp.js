1f5ebe4df75b5757bf868c49da2952777bd6219e
Runaal Parmar <runaalparmar@gmail.com>
2022-03-04 03:08:03 -0500

    r3parmar: merging the backend code for the signin/signup page with the backend code for the registration page.

---
+++ public/signInSignUp/src/signInSignUp.js
@@ -0,0 +1,70 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+
+const realSignUpButton = document.getElementById('signUp');
+const signUpForm = document.getElementById('signUpForm');
+const realSignInButton = document.getElementById('signIn');
+const signInForm = document.getElementById('signInForm');
+
+const container = document.getElementById('container');
+
+const localhost_addr = 'http://localhost:5000';
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+signUpForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = {};
+
+    for (const child of signUpForm.querySelectorAll('.signUpFields')) {
+        formData[child.id] = child.value;
+    }
+
+    fetch(localhost_addr + '/users/signUp', {
+        method: 'POST',
+        headers: {
+        	'Accept': 'application/json',
+        	'Content-Type': 'application/json'
+    	},
+        body: JSON.stringify(formData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('New User Info', data)
+        })
+        .catch((error) => {
+            console.error('Error:', error)
+        })
+});
+
+signInForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const signInEmail = document.getElementById('signInEmail').value;
+    const signInPassword = document.getElementById('signInPassword').value;
+
+    const formData = {email: signInEmail, password: signInPassword}
+
+    fetch(localhost_addr + '/users/signIn', {
+        method: 'POST',
+        headers: {
+        	'Accept': 'application/json',
+        	'Content-Type': 'application/json'
+    	},
+        body: JSON.stringify(formData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('New User Info', data)
+        })
+        .catch((error) => {
+            console.error('Error:', error)
+        })
+});
---
+++ routes/users.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const {model} = require('mongoose');
+const passport = require('passport');
+const {v4: uuidv4} = require('uuid');
+const {genPassword} = require('../lib/passwordUtils');
+const {isAuth, isAdmin, isSuperAdmin} = require('./authMiddleware');
+const {User} = require('../models/users');
+const router = express.Router();
+
+router.get("/getusers", async (req, res) => {
+  const posts = await User.find();
+  res.send(posts);
+});
+
+router.get("/getUsers/:id", async (req, res) => {
+  try {
+    const post = await User.findOne({ _id: req.params.id });
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "User doesn't exist!" });
+  }
+});
+
+router.patch("/patchUser/:id", async (req, res) => {
+  try {
+    const post = await User.findOne({ _id: req.params.id });
+
+    if (req.body.title) {
+      post.title = req.body.title;
+    }
+
+    if (req.body.content) {
+      post.content = req.body.content;
+    }
+
+    await post.save();
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "User doesn't exist!" });
+  }
+});
+
+router.delete("/deleteUser/:id", async (req, res) => {
+  try {
+    await User.deleteOne({ _id: req.params.id });
+    res.status(204).send();
+  } catch {
+    res.status(404);
+    res.send({ error: "User doesn't exist!" });
+  }
+});
+
+router.post("/createUser", async (req, res) => {
+  const post = new User({
+    firstname:req.body.firstname,
+    surname:req.body.surname,
+    DOB:req.body.DOB,
+    address:req.body.address,
+    zip:req.body.zip,
+    gender:req.body.gender,
+    phoneNumber:req.body.phoneNumber
+  });
+  await post.save();
+  res.send(post);
+});
+
+router.post('/signIn', passport.authenticate('local', {
+    failureRedirect: '/login-failure',
+    successRedirect: '/login-success',
+})); // TODO: change redirects.
+
+
+router.post('/signUp', (req, res, next) => {
+    const saltHash = genPassword(req.body.password);
+
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
+
+    const uid = uuidv4();
+
+    // Check if email already registered.
+    User.findOne({email: req.body.email}).then((user) => {
+        if (user) {
+            // Email exists.
+            res.status(403).send("email already exists");
+        
+        } else {
+            // Create new user.
+            const newUser = new User({
+                uid,
+                email: req.body.email,
+                orgName: req.body.orgName,
+                username,
+                hash, // TODO
+                salt, // TODO
+                authLevel: "admin" // TODO
+            });
+
+            newUser.save()
+                .then((user) => {
+                    console.log(user);
+                    next();
+                });
+        }
+    })
+}, passport.authenticate('local', {failureRedirect: 'login-failure', successRedirect: 'login-success'}));
+
+module.exports = router;
