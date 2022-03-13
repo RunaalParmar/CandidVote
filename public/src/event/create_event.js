7ad7950cfc1940a0bf1f1d51b8ac6ca478c1df30
Walter Okpala <cwokpala@uwaterloo.ca>
2022-03-12 23:25:50 -0500

    Added the json format

--- public/src/event/create_event.js
+++ public/src/event/create_event.js
@@ -12,21 +12,26 @@
    function addCandidate(){
       let name = document.createElement('div');
       name.classList.add('main_div')
-      
+      let delete_btn = document.createElement('button')
+      delete_btn.innerHTML ="delete"
+      delete_btn.setAttribute('id', 'delete_btn')
+      delete_btn.setAttribute("type", "button")
+      delete_btn.setAttribute("onclick" , "this.parentElement.remove();")
+      delete_btn.classList.add('delete_b')
       let holderOne = document.createElement('div')
       holderOne.classList.add('holder')
       
       
       let lableOne = document.createElement('span')
-      lableOne.innerHTML = 'Full Name'
+      lableOne.innerHTML = 'Name'
       lableOne.classList.add('on_touch')
       
   
       let inputBox1 = document.createElement('input')
       inputBox1.setAttribute("type", "text");
       inputBox1.value = candidateName.value
-      inputBox1.disabled = true;
-      inputBox1.classList.add('eventField')
+      
+      // inputBox1.classList.add('eventField')
       inputBox1.classList.add('box')
       
       holderOne.appendChild(lableOne)
@@ -47,8 +52,8 @@
       let inputBox2 = document.createElement('input')
       inputBox2.setAttribute("type", "email");
       inputBox2.value = candidateEmail.value
-      inputBox2.disabled = true;
-      inputBox2.classList.add('eventField')
+      
+      // inputBox2.classList.add('eventField')
       inputBox2.classList.add('box')
       
       holderTwo.appendChild(lableTwo)
@@ -68,8 +73,8 @@
       let inputBox3 = document.createElement('input')
       inputBox3.setAttribute("type", "text");
       inputBox3.value = candidateParty.value
-      inputBox3.disabled = true;
-      inputBox3.classList.add('eventField')
+      
+      // inputBox3.classList.add('eventField')
       inputBox3.classList.add('box')
       
       holderThree.appendChild(lableThree)
@@ -83,34 +88,28 @@
       holderFour.classList.add('holder')
       
       let lableFour = document.createElement('span')
-      lableFour.innerHTML = 'Candidate Manifesto'
+      lableFour.innerHTML = 'Platform'
       lableThree.classList.add('on_touch')
       
   
       let inputBox4 = document.createElement('textarea')
       inputBox4.value = candidateDescripttion.value
-      inputBox4.disabled = true;
-      inputBox4.classList.add('eventField')
+      
+      // inputBox4.classList.add('eventField')
       inputBox4.classList.add('box1')
       
       holderFour.appendChild(lableFour)
       holderFour.appendChild(inputBox4)
 
       
-      
+      name.appendChild(delete_btn)
       name.appendChild(holderOne)
       name.appendChild(holderTwo)
       name.appendChild(holderThree)
       name.appendChild(holderFour)
       
-      
-      
-      
-      
       return name
    }
-   
-   
    
      candidateForm.addEventListener('submit' , (event) =>{
      event.preventDefault()
@@ -119,37 +118,52 @@
      document.getElementById('add').checked = false;
    })
    
-    const formData = {}
-    
-    for(const child of eventForm.querySelectorAll('.eventField'))
-    {
-      formData[child.id] = child.value;
-    }
+
     
     eventForm.addEventListener('submit' , (event) => {
       
       event.preventDefault();
+      const formData = {candidates:[]}
+    
+      for(const child of eventForm.querySelectorAll('.eventField'))
+      {
+        formData[child.id] = child.value.trim();
+      }
       
-      fetch(local_addr + '/user/event' ,{
+      
+      for(const child of eventForm.querySelectorAll('.main_div')){
+        const candidate = {}
+        for(const baby of child.childNodes){
+          if( baby.innerText === 'delete') continue
+          candidate[baby.innerText] = baby.lastChild?.value.trim()
+        }
+        formData.candidates.push(candidate)
+      }
+      // {
+      //   formData[child.id] = child.value;
+      // }
+      
+        console.log(formData)
+      // fetch(local_addr + '/user/event' ,{
         
-        method:'POST',
-        headers:{
-          "Accept": "application/json",
-          "Content-Type": "application/json"
-        },
-        body:JSON.stringify(formData)
+      //   method:'POST',
+      //   headers:{
+      //     "Accept": "application/json",
+      //     "Content-Type": "application/json"
+      //   },
+      //   body:JSON.stringify(formData)
         
-      }).then(response => {
+      // }).then(response => {
         
         
-        response.json()
+      //   response.json()
         
-      }).then(data => {
-        console.log(data)
-        candidates,innerHTML ="";
-      }).catch((error) => {
-        console.error('Error' , error)
-      })
+      // }).then(data => {
+      //   console.log(data)
+      //   candidates,innerHTML ="";
+      // }).catch((error) => {
+      //   console.error('Error' , error)
+      // })
       
       
     })
