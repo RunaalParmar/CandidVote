b6fc2f41bb5614b1cc4e9c09e8bcc10c0b943f20
Walter Okpala <cwokpala@uwaterloo.ca>
2022-03-23 23:48:42 -0400

    okpala: Added a link to the add event button

---
+++ public/adminDashboard/dashboard_admin.js
@@ -0,0 +1,7 @@
+(function (params) {
+    const addEvent = document.getElementById('addEvent')
+    
+    addEvent.addEventListener('click', ()=>{
+        window.location.replace("../creatEvent/creatEvent.html")
+    })
+})()
